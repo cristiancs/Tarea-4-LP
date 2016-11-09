5ba4fae82e0b2ff28df76480d1edf9fcc053c9b7
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-11-09 20:07:20 -0300

    terminado 3

--- ejercicio-3.rkt
+++ ejercicio-3.rkt
@@ -1,17 +1,25 @@
 #lang scheme
 
+;;(status 1)
+;;Lleva la cuenta de si se perdio el equilibrio
+(define status 1)
+
+;;(lizq lista)
+;;Obtiene el hijo izquierdo del arbol en caso de corresponder
 (define (lizq lista)
   (if (null? (cdr lista))
       '()
       (car (cdr lista))
-   )
+      )
   )
 
+;;(lder lista)
+;;Obtiene el hijo derecho del arbol en caso de corresponder
 (define (lder lista)
   (cond ((null? (cdr lista)) (cdr lista))
-    ((null? (cdr (cdr lista))) (cdr (cdr lista)))
-      (else (car (cdr (cdr lista))))
-   )
+        ((null? (cdr (cdr lista))) (cdr (cdr lista)))
+        (else (car (cdr (cdr lista))))
+        )
   )
 
 (define pesomovil
@@ -19,15 +27,16 @@
     (if (null? lista)
         0
         (let pesos ((izq 0) (der 0) (head 0) (i 0))
-          (cond ((= i 0) (pesos
-               (pesomovil (lizq lista))
-               (pesomovil (lder lista))
-               (car lista)
-               (+ i 1)
-               ))
-                ((not (= izq der)) '())
+          (cond ((= status 0) '())
+                ((= i 0) (pesos
+                          (pesomovil (lizq lista))
+                          (pesomovil (lder lista))
+                          (car lista)
+                          (+ i 1)
+                          ))
+                ((not (= izq der)) (set! status 0))
                 ((= i 1) (+ head izq der))
-              )
+                )
           )
         )
     )
