9a33f7e6cb4fe1bf287480c21e3cb900ed7c385b
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-11-09 15:45:55 -0300

    actualizado 7

--- ejercicio-7.rkt
+++ ejercicio-7.rkt
@@ -4,15 +4,12 @@
 ;;Lleva la cuenta de la paridad actual
 (define paridad 0)
 
-;;(duplicar x)
-;;Calcula el doble de un numero, si se ha llamado un numero impar de veces
-(define (duplicar x)
-  (define (impar n)
+(define (impar f)
+  (lambda (x)
     (set! paridad (modulo (+ paridad 1) 2))
     (if (= (modulo paridad 2) 0)
-        n
-        (* n 2)
+        x
+        (f x)
         )
     )
-  (impar x)
   )
